93fae3a55bed8221db31aa041f9ce8bf40064e0d
Ulgbbek-js <dilafruztoshmatova209@gmail.com>
2025-03-27 12:27:36 +0500

    style berdim 2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 function Products({ phone, isPending }) {
   console.log(phone)
   return (
-    <div className="container ml-auto justify-center">
+    <div className="container ml-auto justify-center w-6xl">
       {isPending && <h2>Lodaing ...</h2>}
     <div className=" grid grid-cols-3">
     { phone.data.map((p)=>{
